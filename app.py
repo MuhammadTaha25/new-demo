467ba60cbb0f5eb53ed95e2d18dc065d58ec5810
MuhammadTaha25 <mtahashahid230@gmail.com>
2025-01-19 16:33:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from langchain.chat_models import ChatOpenAI
-import openai
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_pinecone import PineconeVectorStore
@@ -14,11 +13,8 @@ from operator import itemgetter
 from dotenv import load_dotenv
 import bs4 
 from bs4 import SoupStrainer
-#from langchain_openai import OpenAIEmbeddings
 load_dotenv()
-#openai.api_key = os.getenv("OPENAI_API_KEY")#Document loader
-#print("OPENAI_API_KEY:", os.getenv("OPENAI_API_KEY"))
-#PINECONE_API_KEY=os.getenv('PINECONE_API_KEY ')
+
 @st.cache_data
 def load_document_loader():
     loader = WebBaseLoader(
@@ -34,16 +30,7 @@ chunks=load_document_loader()
 # Initialize embedding and Qdrant
 embed = HuggingFaceEmbeddings(model_name='BAAI/bge-small-en-v1.5')
 
-# embeddings = OpenAIEmbeddings(
-#     model="text-embedding-3-small",
-#     openai_api_key=openai.api_key,
-#     # With the `text-embedding-3` class
-#     # of models, you can specify the size
-#     # of the embeddings you want returned.
-#     # dimensions=1024
-# )
-    # of the embeddings you want returned.
-    # dimensions=1024
+
 # Qdrant setup
 api_key = os.getenv('qdrant_api_key')
 url = 'https://1328bf7c-9693-4c14-a04c-f342030f3b52.us-east4-0.gcp.cloud.qdrant.io:6333'
@@ -54,11 +41,7 @@ doc_store = QdrantVectorStore.from_existing_collection(
     prefer_grpc=True,
     collection_name="Elon Muske"
 )
-# pineconedb=PineconeVectorStore.from_existing_index(index_name='project1', embedding=embeddings)
-# LLM = ChatOpenAI(
-#                 model_name='gpt-4o-mini',
-#                 openai_api_key=openai.api_key,
-#                 temperature=0)
+
 # Initialize Google LLM
 google_api = os.getenv('google_api_key')
 llm = GoogleGenerativeAI(model="gemini-1.5-flash-002", google_api_key=google_api)
@@ -66,7 +49,6 @@ llm = GoogleGenerativeAI(model="gemini-1.5-flash-002", google_api_key=google_api
 # Setup retriever and chain
 
 num_chunks = 5
-#retriever = pineconedb.as_retriever(search_type="mmr", search_kwargs={"k": num_chunks})
 retriever =doc_store.as_retriever(search_type="mmr", search_kwargs={"k": num_chunks})
 
 def format_docs(docs):
